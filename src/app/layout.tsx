bb8f71f8d1804d1726b13ec1dd7efeea9531f9ea
merge <fpel8800@gmail.com>
2024-11-13 03:53:03 +0900

    feat: 네이버 지도 API 연결

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css';
 import Providers from '@/app/Providers';
+import Script from 'next/script';
 
 export default function RootLayout({
   children,
@@ -8,7 +9,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <title>소모임</title>
+      </head>
       <body>
+        <Script
+          src="https://oapi.map.naver.com/openapi/v3/maps.js?ncpClientId=bjxx1czzob"
+          strategy="beforeInteractive"
+        />
         <Providers>{children}</Providers>
       </body>
     </html>
